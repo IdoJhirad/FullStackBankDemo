348ee1bf2c95a0c1c0c5448c6e5189b7654ea0e9
IDO JHIRAD <IDOJHIRAD@GMAIL.COM>
2025-04-02 11:24:31 +0300

    hebrew

--- Work_Bank_Api/Dtos/CreateTtansactionDto.cs
+++ Work_Bank_Api/Dtos/CreateTtansactionDto.cs
@@ -7,12 +7,12 @@ namespace Work_Bank_Api.Dtos
     {
         [Required]
         [MaxLength(20, ErrorMessage = " Name Should Contanin no more than 20 Character.")]
-        [RegularExpression("^[\\u05D0-\\u05EA\\s'-]{1,20}$",ErrorMessage = "HebrewName should contain only hebrew laters")]
+        [RegularExpression("^(?=.*[\\u05D0-\\u05EA])[\\u05D0-\\u05EA\\s'-]{1,20}$", ErrorMessage = "HebrewName should contain at least one Hebrew letter and only contain allowed characters.")]
         public string HebrewName { get; set; } = string.Empty;
        
         [Required]
         [MaxLength(15, ErrorMessage = " Name Should Contanin no more than 20 Character.")]
-        [RegularExpression("^[A-Za-z\\s'-]{1,15}$",ErrorMessage = "EnglishName should contain only english latters")]
+        [RegularExpression("^(?=.*[[A-Za-z])[A-Za-z\\s'-]{1,15}$", ErrorMessage = "EnglishName should contain only english latters")]
         public string EnglishName { get; set; } = string.Empty;
         
         [Required]
